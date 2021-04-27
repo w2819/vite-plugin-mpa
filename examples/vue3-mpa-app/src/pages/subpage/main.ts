8f2379031369a70d797478ed9e4588224294f6e7
warden <gjs@qq.com>
2021-04-27 11:20:49 +0800

    'update'

--- examples/vue3-mpa-app/src/pages/subpage/main.ts
+++ examples/vue3-mpa-app/src/pages/subpage/main.ts
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router';
+import test from '/src/pages/subpage/test.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/test',
+            name: 'test',
+            component: test,
+            meta: {
+                title: '首页',
+                noLogin: true
+            }
+        },
+    ],
+    strict: true,
+});
+
+const app = createApp(App)
+app.use(router);
+router.isReady().then(() => {
+    app.mount('#app', true);
+})
+
